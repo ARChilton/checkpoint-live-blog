001c1ec32c2ff604e35bc8d0c555de45a9e22b7b
ARChilton <adam@dsan.co.uk>
2018-05-03 14:38:24 -0400

    updated link container to be flex

--- src/components/LinkContainer/LinkContainer.js
+++ src/components/LinkContainer/LinkContainer.js
@@ -1,6 +1,11 @@
 import styled from 'react-emotion'
 
-const LinkContainer = styled('div') `
+const LinkContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: nowrap;
   min-height: 100px;
   padding: 2vh 16px;
   border-bottom: 1px solid #efefef;
