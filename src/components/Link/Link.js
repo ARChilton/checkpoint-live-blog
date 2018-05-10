5b34198383f529eb2bd7f12ccc3c910489185900
ARChilton <adam@dsan.co.uk>
2018-05-10 12:54:11 -0400

    updated to allow internal links to be turned off in CMS preview mode

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import GatsbyLink from 'gatsby-link'
 
-const Link = ({ children, to, href, ...other }) => {
+const Link = ({ children, to, href, linksOn, ...other }) => {
   // Tailor the following test to your environment.
   // This example assumes that any internal link (intended for Gatsby)
   // will start with exactly one slash, and that anything else is external.
   const internal = /^\/(?!\/)/.test(to)
 
   // Use gatsby-link for internal links, and <a> for others
-  if (!internal) {
+  if (!internal || !linksOn) {
     const { exact, activeStyle, ...safeProps } = other
     return (
       <a href={to} {...safeProps}>
@@ -28,11 +28,13 @@ Link.propTypes = {
   children: PropTypes.node.isRequired,
   to: PropTypes.string,
   href: PropTypes.string,
+  linksOn: PropTypes.bool,
 }
 
 Link.defaultProps = {
   to: null,
   href: null,
+  linksOn: true,
 }
 
 export default Link
