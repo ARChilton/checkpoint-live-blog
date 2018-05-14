602c52cdc2aef3f87763a7bd6f39d3862e2507a9
ARChilton <adam@dsan.co.uk>
2018-05-14 18:35:12 -0400

    added some plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,10 +18,28 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     `gatsby-plugin-catch-links`,
+    `gatsby-plugin-react-next`,
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 786,
+            },
+          },
+        ],
       },
     },
   ],
