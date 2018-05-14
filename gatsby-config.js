8ad3b7c5d764dea5c063bfd68d0dc3a79f71e2d0
ARChilton <adam@dsan.co.uk>
2018-05-14 19:35:56 -0400

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,23 +25,23 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 786,
-            },
-          },
-        ],
-      },
-    },
+    // `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-transformer-remark`,
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: `gatsby-remark-images`,
+    //         options: {
+    //           // It's important to specify the maxWidth (in pixels) of
+    //           // the content container as this plugin uses this as the
+    //           // base for generating different widths of each image.
+    //           maxWidth: 786,
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
   ],
   pathPrefix: '/blog',
 }
